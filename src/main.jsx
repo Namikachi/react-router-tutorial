19a4bbc47ca17a6ece9db6b59c01fd7e4489e775
Panorami <loud.music.n@gmail.com>
2023-03-17 09:20:33 -0400

    second commit

--- src/main.jsx
+++ src/main.jsx
@@ -5,26 +5,66 @@ import {
 	RouterProvider,
 } from "react-router-dom";
 import './index.css';
-import Root from "./routes/root";
+import Root, {
+	loader as rootLoader,
+	action as rootAction,
+} from "./routes/root";
 import ErrorPage from "./error-page";
-import Contact from "./routes/contact";
+import Contact, {
+	loader as contactLoader,
+	action as contactAction,
+} from "./routes/contact";
+import EditContact, {
+	action as editAction,
+} from "./routes/edit";
+import {
+	action as destroyAction
+} from "./routes/destroy";
+import Index from "./routes/index";
 
 const router = createBrowserRouter([
 	{
 		path: "/",
 		element: <Root />,
 		errorElement: <ErrorPage />,
+		loader: rootLoader,
+		action: rootAction,
 		children: [
-      {
-        path: "contacts/:contactId",
-        element: <Contact />,
-      },
-    ],
+			{
+				errorElement: <ErrorPage />,
+				children: [
+					{ index: true, element: <Index /> },
+					{
+						path: "contacts/:contactId",
+						element: <Contact />,
+						loader: contactLoader,
+						action: contactAction,
+					},
+					{
+						path: "contacts/:contactId",
+						element: <Contact />,
+						loader: contactLoader,
+						action: contactAction,
+					},
+					{
+						path: "contacts/:contactId/edit",
+						element: <EditContact />,
+						loader: contactLoader,
+						action: editAction,
+					},
+					{
+						path: "contacts/:contactId/destroy",
+						action: destroyAction,
+						errorElement: <div>Oops! There was an error.</div>,
+					},
+				],
+			},
+		],
 	},
 	{
-    path: "contacts/:contactId",
-    element: <Contact />,
-  },
+		path: "contacts/:contactId",
+		element: <Contact />,
+	},
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
